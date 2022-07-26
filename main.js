10cfe573f796ba36ca444db3b4c55afd1a546107
Pardeep Kumar <veervirvani@gmail.com>
2022-07-26 11:47:52 +0200

    Lecture 5, as per README.md file.

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' } , 
+            ],
+            sizes: [
+                {id:3336, f_size: '33/36'},
+                {id:3639, f_size: '36/39'},
+                {id:3942, f_size: '39/42'},                
+            ]
         }
     }
 })
