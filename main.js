1b4da04d640bdb38a837783b691137c74a6e77ec
Pardeep Kumar <veervirvani@gmail.com>
2022-07-27 13:35:57 +0200

    Lecture 6 updated as per README file.

--- main.js
+++ main.js
@@ -11,5 +11,23 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    }, 
+    // putting a method
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image  = variantImage
+
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+                }
+            else
+                this.cart = 0
+
+        }
     }
 })
