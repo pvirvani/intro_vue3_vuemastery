6449fb2498c0545e3a5fe3a966c8e9a9128fe750
Pardeep Kumar <veervirvani@gmail.com>
2022-07-28 14:01:00 +0200

    Lecture 10 modified as per README file.

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            // cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        },
+        updateCart_remove(id){
+            this.cart.pop(id)
+        }
+    }
 })
